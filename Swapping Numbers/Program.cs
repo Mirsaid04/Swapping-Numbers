c90d0da77b9da17adce9978cf12bf2d0362a5c2a
Mirsaid04 <mirsaidd04@gmail.com>
2023-10-11 16:57:00 +0900

    Add project files.

---
+++ Swapping Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Swapping_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello , Welcome to my application");
+
+            Console.Write("Please enter the first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int temp;
+
+            temp = secondNumber;
+            secondNumber = firstNumber;
+            firstNumber = temp;
+
+            Console.WriteLine("\nProgess is going...\n");
+
+            Console.WriteLine($"FirstNumber is {firstNumber}");
+            Console.WriteLine($"SecondNumber is {secondNumber}");
+        }
+    }
+}
